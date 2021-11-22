88f3888a0f832de5585d70a11e44e2f7fd9f74e3
hamstercat007 <lisamac421@gmail.com>
2021-11-22 03:03:44 +0000

    Creating deposit method

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,2 +1,11 @@
 class BankAccount
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
+  def deposit(deposit:)
+    @balance += deposit
+  end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -7,4 +7,13 @@ describe BankAccount do
       expect(bankaccount).to be_a BankAccount
     end
   end
+
+  describe '.deposit' do
+    it 'will store the amount deposited' do
+      bankaccount = BankAccount.new
+      bankaccount.deposit(deposit: 300)
+      expect(bankaccount.balance).to eq 300
+    end
+  end
+
 end
