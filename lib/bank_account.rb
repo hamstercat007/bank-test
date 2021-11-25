a81bf62d325b0a4c016e1529e6f904e4267ed893
hamstercat007 <lisamac421@gmail.com>
2021-11-25 10:30:58 +0000

    Updating final test

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,15 +1,20 @@
+require 'date'
+
 class BankAccount
   attr_reader :balance
 
   def initialize
     @balance = 0
+    @logs = []
   end
 
-  def deposit(deposit:)
+  def deposit(deposit:, date:)
     @balance += deposit
+    @logs.push(deposit, Date.parse(date), @balance)
   end
 
-  def withdrawal(withdrawn:)
+  def withdrawal(withdrawn:, date:)
     @balance -= withdrawn
+    @logs.push(withdrawn, Date.parse(date), @balance)
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -11,7 +11,7 @@ describe BankAccount do
   describe '.deposit' do
     it 'will store the amount deposited' do
       bankaccount = BankAccount.new
-      bankaccount.deposit(deposit: 300)
+      bankaccount.deposit(deposit: 300, date: '10-11-2021')
       expect(bankaccount.balance).to eq 300
     end
   end
@@ -19,9 +19,20 @@ describe BankAccount do
   describe '.withdrawal' do
     it 'will withdraw the amount from the bank account' do
       bankaccount = BankAccount.new
-      bankaccount.deposit(deposit: 5000)
-      bankaccount.withdrawal(withdrawn: 500)
+      bankaccount.deposit(deposit: 5000, date: '10-11-2021')
+      bankaccount.withdrawal(withdrawn: 500, date: '12-11-2021')
       expect(bankaccount.balance).to eq 4500
+    end
+  end
+
+  describe '.statement' do
+    it 'will print a statement of deposits, withdrawals and balance including dates' do
+      bankaccount = BankAccount.new
+      bankaccount.deposit(deposit: 5000, date: '2021-11-10')
+      bankaccount.deposit(deposit: 100, date: '2021-11-11')
+      bankaccount.withdrawal(withdrawn: 500, date: '2021-11-12')
+      expect(bankaccount.statement).to eq "date || credit || debit || balance\n 12-11-2021 || || 500.00 || 4600.00\n 11/11/2021 || 100.00 || || 5100.00\n 10-11-2021 || 5000.00 || || 5000.00"
+      
     end
   end
 end
